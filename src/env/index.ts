72f61e6f72ae5a9009c49761307a1451bfc3a9a9
abvictor <victor.amaralb@hotmail.com>
2025-07-06 22:02:01 -0300

    chore: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,16 +9,17 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
-  DATABASE_CLIENT: z.enum(['pg', 'sqlite']),
   PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.log('erro:', _env.error)
-  throw new Error('variáveis de ambiente inválidas')
+  console.error('⚠️ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
